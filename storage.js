5175370c0dab451fab73871f5d77e307a8c3f88d
BScherz1 <bscherz1@umbc.edu>
2021-06-29 21:28:03 -0400

    Update to login function with json storage

--- storage.js
+++ storage.js
@@ -4,20 +4,49 @@ function login(){
     var nurse = document.getElementById("nur").value;
     var doctor = document.getElementById("doc").value;
     var role;
+    var user;
     var name = document.getElementById("name").value;
 
-    if(nurse == "Nurse"){
+    
 
-        role = nurse;
-    }else{
+        if(document.getElementById("nur").checked){
 
-        role = doctor;
-    }
+            if(name){
 
-    db[0] = {NAME: name, ROLE: role};
-    var user = JSON.stringify(db);
+                role = nurse;
+                db[0] = {NAME: name, ROLE: role};
+                user = JSON.stringify(db);
 
-    localStorage.setItem("local_list", user);
-    console.log(db);
-    console.log(user);
+                localStorage.setItem("local_list", user);
+                console.log(db);
+                console.log(user);
+            }else{
+
+                alert("Please enter your name to log-in.")
+            }
+        }else if(document.getElementById("doc").checked){
+
+            if(name){
+
+                role = doctor;
+                db[0] = {NAME: name, ROLE: role};
+                user = JSON.stringify(db);
+
+                localStorage.setItem("local_list", user);
+                console.log(db);
+                console.log(user);
+            }else{
+
+                alert("Please enter your name to log-in.")
+            }
+        }else{
+
+            alert("Please select a designation to log-in.");
+        }
+
+    
+
+    
+
+    
 }
