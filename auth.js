82e7178bbfc147a0890feb608c95003bc7cceb34
Connor Markwell <markwell.connor@gmail.com>
2021-07-01 18:38:31 -0400

    Refactor login, Add Main page

---
+++ auth.js
@@ -0,0 +1,19 @@
+function login() {
+    const nurse = document.getElementById("nurse");
+    const doctor = document.getElementById("doctor");
+    const name = document.getElementById("name").value;
+
+    if (!nurse.checked && !doctor.checked) {
+        alert("Please selct a role to log in.");
+        return;
+    }
+
+    if (!name) {
+        alert("Please enter your name to log in.");
+        return;
+    }
+
+    const role = nurse.checked ? nurse.value : doctor.value;
+    const user = { name, role };
+    localStorage.setItem("user", JSON.stringify(user));
+}
