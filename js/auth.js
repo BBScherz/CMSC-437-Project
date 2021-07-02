a74c53d6b58d395ef20f5089cea13f26d1a040ef
Connor Markwell <markwell.connor@gmail.com>
2021-07-01 22:29:20 -0400

    Add comments to auth script

--- js/auth.js
+++ js/auth.js
@@ -1,13 +1,17 @@
+// Try to fetch user from localStorage, get the visited file
 const user = JSON.parse(localStorage.getItem("user"));
 const url = window.location.pathname;
 const filename = url.substring(url.lastIndexOf("/") + 1);
 
+// If user is not found and we are not on the login page, redirect
 if (filename !== "login_page.html" && !user) {
     window.location.href = "login_page.html";
+// Else, if we are on the login page and user is found, redirect
 } else if (filename === "login_page.html" && user) {
     window.location.href = "main_page.html";
 }
 
+// Get checked role & name, create user object for localStorage
 function login() {
     const nurse = document.getElementById("nurse");
     const doctor = document.getElementById("doctor");
@@ -28,6 +32,7 @@ function login() {
     localStorage.setItem("user", JSON.stringify(newUser));
 }
 
+// Remove user from localStorage, redirect
 function logout() {
     localStorage.removeItem("user");
     window.location.href = "login_page.html";
