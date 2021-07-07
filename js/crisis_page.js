0586044a16a252a0fede92a18fffb2d111393c27
KTDR <33167082+KTDR@users.noreply.github.com>
2021-07-06 22:02:32 -0400

    Add write functionality to patient crisis thresholds
    Does not save thresholds if page is refreshed even when save button is clicked, currently trying to fix this.

--- js/crisis_page.js
+++ js/crisis_page.js
@@ -5,12 +5,8 @@ if (!patient) {
 
 
 $(document).ready(function() {
-
-
-    $("#blood-oxygen-threshold").text("Current: 80")
-    $("#minimum-heartrate-threshold").text("Current: 60")
-    $("#maximum-heartrate-threshold").text("Current: 160")
-
+    
+    updateThresholdDisplays();
 
     if (user.role === "Doctor") {
         $("#threshold1-container").append(`
@@ -25,19 +21,42 @@ $(document).ready(function() {
             <input type="text" id="maximum-heartrate-threshold-new" name="threshold-name">
             <input type="button" value="Save" id="add-maximum-heartrate-threshold">
         `);
-
-
     }
+
+    $("#add-blood-oxygen-threshold").click(function () {
+        patient.minimumO2Threshold = $("#blood-oxygen-threshold-new").val();
+        updateThresholdDisplays();
+    });
+    $("#add-minimum-heartrate-threshold").click(function () {
+        patient.minimumHeartrateThreshold = $("#minimum-heartrate-threshold-new").val();
+        updateThresholdDisplays();
+    });
+    $("#add-maximum-heartrate-threshold").click(function () {
+        patient.maximumHeartrateThreshold = $("#maximum-heartrate-threshold-new").val();
+        updateThresholdDisplays();
+        writePatients();
+    });
 
 });
 
-// Save patient data to localStorage
-$(window).on('beforeunload', () => {
+
+function updateThresholdDisplays() {
+    $("#blood-oxygen-threshold").text("Current: " + patient.minimumO2Threshold)
+    $("#minimum-heartrate-threshold").text("Current: " + patient.minimumHeartrateThreshold)
+    $("#maximum-heartrate-threshold").text("Current: " + patient.maximumHeartrateThreshold)
+    writePatients();
+}
+
+// Save patient data if page is navigated away from
+$(window).on('beforeunload', writePatients);
+
+//write data of current patient to the patient storage
+function writePatients() {
     let nextPatients = JSON.parse(localStorage.getItem("patients"));
     nextPatients = patients.map(({ id }, i) => (
         id === patient.id 
             ? patient 
-            : patients[i]
+            : nextPatients[i]
     ));
-    localStorage.setItem("patients", JSON.stringify(nextPatient));
-});
+    localStorage.setItem("patients", JSON.stringify(nextPatients));
+}
